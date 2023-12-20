5515c11c40f5f990bbe9f688098f55ade56109db
Nauris <nauris.valaks@aize.io>
2023-12-20 17:37:55 +0000

    new commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import './App.scss'
 import { observer } from "mobx-react-lite";
 import React from "react";
 import { AppState } from "./AppState";
+
 
 interface AppProps {
   appState: AppState;
@@ -9,8 +11,16 @@ interface AppProps {
 export const App: React.FC<AppProps> = observer(({ appState: AppState }) => {
 
   return (
-    <div>
-      {AppState.content}
+    <div className="content">
+        <ul className='navigation'>
+        {AppState.navigationItems.map(item => {
+          return <li onClick={() => { AppState.handleScrollTo(item.hrefName) }} className='navigation__item'><a className='navigation__link' href={item.hrefName}>{item.linkName}</a></li>
+        })}
+        </ul>
+        <div ref='work'>
+          Testing work
+        </div>
+        <div ref='about'></div>
     </div>
   )
 })
--- src/AppState.ts
+++ src/AppState.ts
@@ -1,8 +1,19 @@
-import { action, makeObservable, observable } from 'mobx';
-
 export const baseLinkUrl = 'https://nauris-valaks.github.io/';
 
-
 export class AppState {
-  public content = 'New Page'
+  public navigationItems = [
+    { linkName: 'Work', hrefName: '#work' },
+    { linkName: 'About', hrefName: '#about' },
+    { linkName: 'Contact', hrefName: '#contact' },
+  ];
+
+
+  handleScrollTo = (elRef: any) => { // use consts or let
+    const el = elRef.current ? elRef.current : elRef
+    
+    el.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  }
 }
